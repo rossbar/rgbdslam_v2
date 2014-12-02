19621483a90515b92266e4d82a6cabc41b1e6f18
Ross Barnowski <rossbar@berkeley.edu>
2014-12-02 10:50:31 -0800

    Added some more to volumetric node.

--- nodes/VolumetricImagingNode.py
+++ nodes/VolumetricImagingNode.py
@@ -6,13 +6,21 @@ import time
 
 import rospy
 
+# Messages
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import TransformStamped
-from python_msg_conversions import pointclouds
-
-#ADD DTYPES
+from rgbdslam.msg import ComptonEventList
+# Message conversion functions
+from python_msg_conversions import pointcloud2_to_xyz_array
+from python_msg_conversions.gammarays import ComptonEventList_to_numpy as ce2np
 
 import tf
+
+from dtype_defines import stamped_transform_type, xyz_cloud_type, edataType
+from converter_functions import convertToRT, convertQuaternionToTuple,\
+                                convertTranslationToTuple
+
+TRANSFORM_BUFFER_SIZE = 10000
 
 class VolumetricImagingNode(object):
   '''This node listens on 3 topics: /unbuffered_poses and /buffered_clouds
@@ -40,3 +48,25 @@ class VolumetricImagingNode(object):
 					     self.compton_callback)
 
   #### Callbacks for appending data
+  def cloud_callback(self, cloud_msg):
+    '''Append xyz positions to point cloud'''
+    self.cloud_counter += 1
+    xyz = pointcloud2_to_xyz_array(cloud_msg, remove_nans=False)
+    self.cloud = np.concatenate(self.cloud, xyz)
+
+  def transform_callback(self, tfmsg):
+    '''Append new transformations'''
+    # Parse message
+    ts = tfmsg.header.stamp.to_nsec()
+    trans = convertTranslationToTuple(tfmsg.transform.translation)
+    rot = convertQuaternionToTuple(tfmsg.transform.rotation)
+    RT = convertToRT(trans, rot)
+    # Append
+    self.transforms['timestamp'][self.pose_counter] = ts
+    self.transforms['RT'][self.pose_counter] = RT
+    self.pose_counter += 1
+
+  def compton_callback(self, event_list):
+    '''Append new Compton data from the detector'''
+    ts, l22 = ce2np(event_list)
+
