a2f5ec74fdabd21a5a20e99fb4a8cdcd1fbf4529
Ross Barnowski <rossbar@berkeley.edu>
2014-12-31 16:18:16 -0800

    Modified the detector publisher to use zmq instead of ros topics.

--- nodes/detector_data_publisher.py
+++ nodes/detector_data_publisher.py
@@ -21,6 +21,15 @@ from src.cythoned.getPtrs_cy import getEventPtrs
 from src.analysisHelperFunctions import interactionType
 from src.CAnalysis.canalysis import analyzeReadoutData
 
+savedInteractionType = np.dtype({"names":['time','energy1', 'x1', 'y1', 'z1', 'dT1',\
+                                         'dE1', 'det1', 'energy2', 'x2', 'y2', 'z2',\
+                                         'dT2', 'dE2', 'det2'],                 
+                                 "formats":[np.float64, np.float32, np.float32,\
+                                            np.float32, np.float32, np.float32,\
+                                            np.float32, np.float32, np.float32,\
+                                            np.float32, np.float32, np.float32,\
+                                            np.float32, np.float32, np.float32]} )
+
 class SISAcquisitionAndAnalysisNode(object):
   '''The SISAcquisitionAndAnalysisNode will insantiate a node that is 
      responsible for acquiring data from a SIS system, '''
@@ -29,11 +38,9 @@ class SISAcquisitionAndAnalysisNode(object):
     self.ctr = 0
     self.msg = None
     self.edata = None
-    self.data_out = None
+    self.data_out = np.empty(0, dtype=savedInteractionType)
     self.rate = rospy.Rate(read_rate)	# Hz, default is every 2 seconds
     self.iary = np.zeros(1, dtype=interactionType) # For CAnalysis
-    # rostopic stuff
-    self.publisher = rospy.Publisher("/compton_events", ComptonEventList)
     # SIS flags - NOTE: HARD-CODED FOR NOW, SHOULD MAKE CONFIGURABLE
     self.save_data = 0	# Default: Don't save binary data
     self.cal_file = PF_DIR + 'src/SIS/CCI2_Calibration_4-9-13.txt'
@@ -44,6 +51,8 @@ class SISAcquisitionAndAnalysisNode(object):
     sis.connectToDAQ()
     sis.configuration(self.conf_file, self.binary_file, self.save_data)
     self.hardware_started = False
+    # Set up ZMQ publisher
+    self.publisher = ZmqEmitter('5556', 'interactions')
 
   def run(self):
     while not rospy.is_shutdown():
@@ -59,9 +68,8 @@ class SISAcquisitionAndAnalysisNode(object):
       # Log stuff
 #      rospy.loginfo(self.msg)
       # Publish
-      if self.data_out is not None:
-        self.publisher.publish(self.data_out)
-#	print 'Published!'
+      if len(self.data_out) > 0:
+        self.publisher.send_zipped_pickle(self.data_out)
 
   def task(self):
     if self.hardware_started:
@@ -69,14 +77,9 @@ class SISAcquisitionAndAnalysisNode(object):
 #      print ts
       self.preprocess_data()
       l22 = self.reconstruct_compton_events()
-      print 'l22 len = ', len(l22)
-#      print l22
       # Format data for sending via ros
       if len(l22) > 0:
-        self.data_out = n2cel(l22, ts=ts)
-	print self.data_out.header.stamp, self.data_out.num_events
-#	print self.data_out
-      else: self.data_out = None
+        self.send_interactions(l22, ts)
     else:
       msg = '[SIS_NODE]: Hardware not started, awaiting signal...'
 
@@ -131,103 +134,37 @@ class SISAcquisitionAndAnalysisNode(object):
     self.start_time = time.time()
     self.hardware_started = True
 
-class DetectorDataPublisher_FromTxtNode(object):
-  '''The FromTxt node reads data from a text file '''
-
-#class SimulatedAcquisitionAndAnalysisNode(object):
-#  '''Same functionality as SIS version, but uses saved HDF5 archive to produce
-#     data rather than the SIS connection.'''
-#  def __init__(self, verbose=True, read_rate=0.5):
-#    # Node attributes
-#    self.ctr = 0
-#    self.msg = None
-#    self.edata = None
-#    self.data_out = None
-#    self.rate = rospy.rate(read_rate)	# Hz, default is every 2 seconds
-#    self.iary = np.zeros(1, dtype=interactionType) # For CAnalysis
-#    # rostopic stuff
-#    self.publisher = rospy.Publisher("/compton_events", ComptonEventList)
-#    # Simulated data flags NOTE: HARD-CODED FOR NOW, SHOULD MAKE CONFIGURABLE
-#    self.fname = '/home/grim5/Desktop/Misc/POST_CALIBRATION_CS_TEST.h5'
-#
-#  def run(self)
-#    self.initialize()
-#    while not rospy.is_shutdown():
-#      # Do task
-#      self.task()
-#      # Log stuff
-#      if self.msg is not None: rospy.loginfo(self.msg)
-#      # Publish
-#      if self.data_out is not None:
-#        self.publisher.publish(self.data_out)
-#      # Wait for next dump
-#      self.rate.sleep()
-#
-#  def task(self):
-#    ts = self.acquire_data()
-#    self.preproces_data()
-#    l22 = self.reconstruct_compton_events()
-#    # Format data for sending via ros
-#    self.data_out = n2cel(l22, ts)
-#
-#  def acquire_data(self):
-#    # Acquire data from sis
-#    tic = time.time()
-#    ts, en, ch, trig = sis.acquiredata(self.cal_file, self.save_data)
-#    acq_ts = time.time()	# Acquisition time stamp
-#    # Convert to numpy array
-#    edata = np.zeros(ts.shape, dtype=edataType)
-#    edata = edata.view(np.recarray)
-#    edata.timestamp = ts
-#    edata.ADC_value = en
-#    edata.detector = ch
-#    edata.trigger = trig
-#    # Set up data for further processing
-#    self.edata = edata
-#    self.ctr += 1
-#    toc = time.time()
-#    # Add logging/printing here
-#
-#    return acq_ts
-#
-#  def preprocess_data(self):
-#    # After having acquired new data, preprocess it to get it ready for
-#    # reconstruction
-#    #NOTE: Filters are all hard-coded here
-#    if self.edata is None: return
-#    # Trigger filter (reject NN and pileup)
-#    self.edata = self.edata[self.edata['trigger'] == 1]
-#    # LLD
-#    self.edata = self.edata[self.edata['ADC_value'] >= 25]
-#    # Reject GR signals
-#    self.edata = self.edata[self.edata['detector'] % 38 != 0]
-#    # Sort
-#    self.edata.sort(order='timestamp')
-#
-#  def reconstruct_compton_events(self):
-#    # Take the preprocessed data and reconstruct all the 2-interaction events
-#    if self.edata is None: return
-#    # Get time-correlated readouts - NOTE: Only looking for 2-int events!
-#    ptrs, lens = getEventPtrs(self.edata['timestamp'], 40, 4, 4)
-#    l11, l22, l33, lerr = analyzeReadoutData(self.edata, ptrs, lens, self.iary)
-#    l22 = l22.reshape((l22.shape[0]/2, 2))
-#    # Set the data to be sent out
-#    return l22
-#
-#  def initialize(self):
-#    self.hf = tables.openFile(self.fname, 'r')
-#    dets = self.hf.root.EventData.col('detector')
-#    self.dumpPtrs = np.array([0] + list(getPtrs_cy.getPtrsToDumps(dets,\
-#		                   10000000)))
-#    self.nextDump = 0
-#    print 'File open and initialized!'
-#
-#  def cleanup(self):
-#    self.hf.close()
-#    print 'HDF5 Archive closed'
+  def send_interactions(self, inter22, acq_ts):
+    # Convert to proper format
+    numEvents = len(inter22)
+    newEvs = np.zeros(numEvents, dtype=savedInteractionType)
+    for i in xrange(numEvents): 
+      int1 = inter22[i][0] 
+      int2 = inter22[i][1]
+      newEvs[i]['time'] = acq_ts 
+      newEvs[i]['energy1'] = int1[0]
+      newEvs[i]['energy2'] = int2[0]
+      newEvs[i]['x1'] = int1[1] 
+      newEvs[i]['x2'] = int2[1] 
+      newEvs[i]['y1'] = int1[2] 
+      newEvs[i]['y2'] = int2[2] 
+      newEvs[i]['z1'] = int1[3]
+      newEvs[i]['z2'] = int2[3]
+      newEvs[i]['dT1'] = int1[4]
+      newEvs[i]['dT2'] = int2[4]
+      newEvs[i]['dE1'] = int1[5]
+      newEvs[i]['dE2'] = int2[5]
+      newEvs[i]['det1'] = int1[6]
+      newEvs[i]['det2'] = int2[6]
+    # Append new interactions to the existing ones
+    self.data_out = np.concatenate((self.data_out, newEvs)) 
+    self.send_ctr += 1 
+    # Send all interactions                                                     
+    self.emitter.send_zipped_pickle(self.data_out)
 
 if __name__ == '__main__':
   rospy.init_node('detector_publisher', anonymous=True)
   detector_publisher = SISAcquisitionAndAnalysisNode()
+  raw_input('Press Enter to begin detector acquisition: ')
+  print 'Starting detector acquisition...'
   detector_publisher.run()
-#  rospy.spin()
